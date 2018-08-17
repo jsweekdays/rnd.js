4234a9c01505e7210ef5dd1fcfa0663005bde9ed
Ivakhnenko Dmitry <jeetiss@yandex.ru>
2018-08-17 19:12:40 +0300

    add 404 (#8)

---
+++ src/pages/404.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import styled, { ThemeProvider } from 'styled-components'
+import { Error404 } from '../components/BgWithLogo'
+import theme from '../utils/theme'
+import addGlobal from '../global-styles'
+
+addGlobal()
+
+const John = styled.img`
+  margin: 0 auto;
+  display: block;
+`
+
+const App = () => (
+  <ThemeProvider theme={theme}>
+    <Error404>
+      <Helmet>
+        <title>404 | RND {'<'}3 JS</title>
+        <meta name='description' content='Ничегошеньки не нашли' />
+      </Helmet>
+      <John
+        src='https://media.giphy.com/media/jWexOOlYe241y/giphy.gif'
+        alt=''
+      />
+    </Error404>
+  </ThemeProvider>
+)
+
+export default App
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,46 +6,10 @@ import BgWithLogo from '../components/BgWithLogo'
 import Card from '../components/Card'
 import Button from '../components/Button'
 import { Header } from '../components/Typography'
+import theme from '../utils/theme'
 import addGlobal from '../global-styles'
 
 addGlobal()
-
-const shadow = 'rgba(13, 8, 26, 0.3)'
-const theme = {
-  colors: {
-    primary: Object.assign('#6624FF', {
-      ligther: 'hsl(258, 95%, 60%)'
-    }),
-    secondary: '#FF3357',
-    tertiary: 'rgba(101, 37, 255, 0.2);',
-    // tertiary: '#F6F7FD',
-
-    font: '#262626',
-    shadow,
-
-    background: ['#262626', '#1C1B1F']
-    // background: ['hsl(255, 7%, 40%)', 'hsl(255, 7%, 45%)']
-  },
-
-  card: {
-    size: ['auto', 'auto', 880, 1104],
-    margin: ['0 8px 25vh 0', '0 16px 25vh 0', ' 0px auto 25vh']
-  },
-
-  shadows: [
-    `8px 16px 0px ${shadow}`,
-    `16px 24px 0px ${shadow}`,
-    `24px 32px 0px ${shadow}`,
-    `32px 48px 0px ${shadow}`
-  ],
-
-  offsets: [
-    'translate(8px, 16px)',
-    'translate(16px, 24px)',
-    'translate(24px, 32px)',
-    'translate(32px, 48px)'
-  ]
-}
 
 const App = () => (
   <ThemeProvider theme={theme}>
