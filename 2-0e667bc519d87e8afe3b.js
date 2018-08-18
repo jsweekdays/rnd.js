9875d3d38d5a1adca04c99e9e8eac10338d5c39e
jeetiss <za5rubley@gmail.com>
2018-08-18 22:26:31 +0300

    Updates

--- 2-0e667bc519d87e8afe3b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{191:function(a){a.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E","dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E"}}}}]);
-//# sourceMappingURL=2-0e667bc519d87e8afe3b.js.map
