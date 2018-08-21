e8add7c908eebfcd8bff96f0938bfc6880a27ca3
Ivakhnenko Dmitry <jeetiss@yandex.ru>
2018-08-21 13:46:03 +0300

    Puppeteer (#11)
    * fixes
    
    * add script for generating images

---
+++ scripts/generate.js
@@ -0,0 +1,43 @@
+const handler = require('serve-handler')
+const puppeteer = require('puppeteer')
+const http = require('http')
+
+const serve = (port = 3000) =>
+  new Promise((resolve, reject) => {
+    const server = http.createServer((request, response) => {
+      return handler(request, response, {
+        public: './public'
+      })
+    })
+
+    server.listen(port, () => {
+      resolve({
+        port,
+        close: () => new Promise(resolve => server.close(resolve))
+      })
+    })
+  })
+
+const generate = async images => {
+  const { port, close } = await serve()
+
+  const browser = await puppeteer.launch()
+
+  await Promise.all(
+    images.map(({ url, fileName, size }) =>
+      browser.newPage().then(async page => {
+        const [width, height] = size.split('x').map(val => Number.parseInt(val))
+
+        await page.setViewport({ width, height, deviceScaleFactor: 1 })
+
+        await page.goto(`http://localhost:${port}${url}`)
+        await page.screenshot({ path: `./images/${fileName}.png` })
+      })
+    )
+  )
+
+  await browser.close()
+  await close()
+}
+
+module.exports = generate
--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ import theme from '../utils/theme'
 const Card = system(
   {
     width: ['auto', 'auto', 808, 1104],
-    m: ['0 8px 25vh 0', '0 16px 25vh 0', '0px auto 25vh'],
+    m: '0px auto 25vh',
     boxShadow: theme.shadows
   },
   () => ({
