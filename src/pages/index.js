09b4c9896c953b78a484aaea3ade67dbc69404c9
jeetiss <za5rubley@gmail.com>
2018-08-27 17:40:17 +0300

    add speakers to landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,10 @@ import {
 } from '../components/Typography'
 import theme from '../utils/theme'
 import addGlobal from '../utils/global-styles'
+import Speaker from '../components/Speaker'
+import Accordion from '../components/Accordion'
+
+import { speakers } from '../../data/speakers.json'
 
 addGlobal()
 
@@ -25,11 +29,17 @@ const App = () => (
       </Helmet>
 
       <Card>
-        <Flex pt={100} pb={90} px={112} flexDirection='column'>
+        <Flex
+          pt={100}
+          pb={90}
+          px={[32, 48, 60, 112]}
+          flexDirection='column'
+        >
           <Box pb={64}>
-            <Text fontSize={42}>
+            <Text fontSize={[24, 36, 36, 42]}>
               Ростов-на-Дону&nbsp;
-              <Colorize color='#FF3357'>/</Colorize> Суворова, 91 этаж&nbsp;7&nbsp;
+              <Colorize color='#FF3357'>/</Colorize> Суворова,&nbsp;91
+              этаж&nbsp;7&nbsp;
               <Colorize color='#FF3357'>/</Colorize> Южный ИТ-парк
             </Text>
           </Box>
@@ -38,7 +48,7 @@ const App = () => (
             <Box>
               <BorderedText
                 fontWeight={900}
-                fontSize={100}
+                fontSize={[36, 36, 64, 100]}
                 fill='white'
                 width={'2px'}
                 stroke='#6624FF'
@@ -48,11 +58,33 @@ const App = () => (
             </Box>
 
             <Box>
-              <Text fontWeight={900} color='#6624FF'>
+              <Text
+                fontWeight={900}
+                fontSize={[36, 36, 64, 100]}
+                color='#6624FF'
+              >
                 15:00
               </Text>
             </Box>
           </Flex>
+        </Flex>
+      </Card>
+
+      <Card>
+        <Flex flexDirection='column' py={[0, 0, 42, 84]}>
+          <Accordion>
+            {({ active, set }) =>
+              speakers.map((speaker, index) => (
+                <Speaker
+                  key={index}
+                  speaker={speaker}
+                  on={index === active}
+                  number={index + 1}
+                  toggle={() => set(index)}
+                />
+              ))
+            }
+          </Accordion>
         </Flex>
       </Card>
 
