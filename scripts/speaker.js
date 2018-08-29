0f0d1e231957de8e918fbc43d6a14864de145b07
Den Ilin <followdarko@gmail.com>
2018-08-29 15:12:14 +0300

    Tune speaker logo sizes (#18)
    Tune generate script not to conflict with other workflows

--- scripts/speaker.js
+++ scripts/speaker.js
@@ -4,5 +4,5 @@ const {speakers} = require('../data/speakers.json')
 generate(speakers.map((_, index) => ({
   url: `/speaker?index=${index}`,
   fileName: `speaker-${index}`,
-  size: '1200x900'
+  size: '900x500'
 })))
